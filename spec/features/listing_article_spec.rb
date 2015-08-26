887f16dff147dc509146cbcaf4f9ce1e6899fb56
Justin Joseph <justin.joseph0@yahoo.com>
2015-08-26 21:18:07 +0000

    Restrict New, Edit & Delete for articles

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -10,6 +10,8 @@ RSpec.feature "Listing Articles" do
   scenario "List all articles" do
     visit "/"
     
+    expect(page).not_to have_link "New Article"
+    
     expect(page).to have_content @article1.title
     expect(page).to have_content @article1.body
     
